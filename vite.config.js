3dd58f5b28430350586836de377773631f8af685
Hemanthreddy01 <120013550+Hemanthreddy01@users.noreply.github.com>
2025-04-27 17:33:50 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   css: {
+//     postcss: './postcss.config.js',
+//   },
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
